61a8f0bb24976e5d56661d943c48c1cb543df455
fjuarez <fdxdesarrollos@gmail.com>
2023-06-06 15:19:47 -0600

    Se agregó CORS al controlador

--- src/main/java/net/fdxdesarrollos/inventario/controller/CategoriaRestController.java
+++ src/main/java/net/fdxdesarrollos/inventario/controller/CategoriaRestController.java
@@ -2,6 +2,7 @@ package net.fdxdesarrollos.inventario.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import net.fdxdesarrollos.inventario.model.Categoria;
 import net.fdxdesarrollos.inventario.response.CategoriaResponseRest;
 import net.fdxdesarrollos.inventario.services.ICategoriaService;
 
+
+@CrossOrigin(origins = {"http://localhost:4200/inventario-frontend/"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoriaRestController {
